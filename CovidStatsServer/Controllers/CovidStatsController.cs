cf21941dea018c92f8014b2b7df9760179715230
Pavel Bolhovitin <bolhovitins.pavels@gmail.com>
2021-11-24 15:17:35 +0200

    Added covid stats API; Simple fetching covid stats from API; Simple displaying covid stats;

---
+++ CovidStatsServer/Controllers/CovidStatsController.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+
+namespace CovidStatsServer.Controllers
+{
+    public class CovidStatsController : ApiController
+    {
+        public class Records
+        {
+            public CovidStat[] records;
+        }
+
+        public class CovidStat
+        {
+            public string dateRep;
+            public string day;
+            public string month;
+            public string year;
+            public int cases;
+            public int deaths;
+            public string countriesAndTerritories;
+            public string geoId;
+            public string countryterritoryCode;  // can be null
+            public string popData2019;  // int, but in json but can null
+            public string continentExp;  // can be empty string
+            [JsonProperty("Cumulative_number_for_14_days_of_COVID-19_cases_per_100000")]
+            public string Cumulative_number_for_14_days_of_COVID19_cases_per_100000;
+        }
+
+
+        // GET api/covidstats
+        public Records Get()
+        {
+            string url = "https://opendata.ecdc.europa.eu/covid19/casedistribution/json/";
+            Records items = null;
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    string response = streamReader.ReadToEnd();
+                    items = JsonConvert.DeserializeObject<Records>(response);
+                }
+            }
+            catch (Exception e)
+            {
+                // TODO: Handle the exception
+            }
+
+            return items;
+        }
+    }
+}
---
+++ CovidStatsServer/Controllers/ValuesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+
+namespace CovidStats.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public class PostObj
+        {
+            public int userId;
+            public int id;
+            public string title;
+            public string body;
+        }
+
+        // GET api/values
+        public List<PostObj> Get()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            List<PostObj> items = null;
+
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                string response = streamReader.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<PostObj>>(response);
+                return items;
+            }
+
+            return items;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
